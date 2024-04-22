97526317d95e46998acab6cf91886d116685565f
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-04-23 00:02:37 +0300

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    // eslint-disable-next-line no-unused-vars, no-shadow
-    var task = true;
-    // eslint-disable-next-line no-unused-vars, no-shadow
-    var task2 = false;
+    let task = true;
+    let task2 = false;
   }
 
   return [task, task2];
