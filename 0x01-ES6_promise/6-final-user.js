4b2ab43d4f47dacb44de418559424a3da606349d
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-04-26 00:46:51 +0300

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ])
-    .then((values) => values.map((val) => ({
-      status: val.status,
-      value: val.status === 'fulfilled' ? val.value : val.reason
-    })))
-    .catch(() => []);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const resp = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    resp.push({ status: 'fulfilled', value: user });
+    await uploadPhoto(fileName);
+  }
+  catch (err) {
+    resp.push({ status: 'rejected', value: `Error ${fileName} cannot be processed` });
+  }
+  return resp;
 }
