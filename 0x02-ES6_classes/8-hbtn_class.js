4434fb7cd7d1c114248713ff26390df75b942d53
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-04-26 23:15:42 +0300

    Update 8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -34,5 +34,6 @@ export default class HolbertonClass {
   [Symbol.toPrimitive](hint) {
     if (hint === 'number') return this._size;
     if (hint === 'string') return this._location;
+    return null;
   }
 }
