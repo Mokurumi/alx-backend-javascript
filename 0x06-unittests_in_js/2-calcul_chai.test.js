81e81037e83d15649216925f78d560a5e4ab29d6
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-06-21 00:45:23 +0300

    update 2

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,28 +1,30 @@
 const { expect } = require('chai');
 const calculateNumber = require('./2-calcul_chai');
 
-describe('calculateNumber', function () {
-  it('should round two numbers and return their sum', () => {
-    expect(calculateNumber('SUM', 1, 3)).to.equal(4);
-    expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
-    expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
-    expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
+describe('calculateNumber', () => {
+  it('should perform addition when type is SUM', () => {
+    const result = calculateNumber('SUM', 1.4, 4.5);
+    expect(result).to.equal(6);
   });
-  it('should round two numbers and return their difference', () => {
-    expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
-    expect(calculateNumber('SUBTRACT', 1, 3.7)).to.equal(-3);
-    expect(calculateNumber('SUBTRACT', 1.2, 3.7)).to.equal(-3);
-    expect(calculateNumber('SUBTRACT', 1.5, 3.7)).to.equal(-3);
+
+  it('should perform subtraction when type is SUBTRACT', () => {
+    const result = calculateNumber('SUBTRACT', 1.4, 4.5);
+    expect(result).to.equal(-4);
   });
-  it('should round two numbers and return their quotient', () => {
-    expect(calculateNumber('DIVIDE', 1, 3.7)).to.equal(0.25);
-    expect(calculateNumber('DIVIDE', 1.2, 3.7)).to.equal(0.25);
-    expect(calculateNumber('DIVIDE', 1.5, 3.7)).to.equal(0.5);
+
+  it('should perform division when type is DIVIDE', () => {
+    const result = calculateNumber('DIVIDE', 1.4, 4.5);
+    expect(result).to.equal(0.2);
   });
-  it('should throw an error if the type is not SUM, SUBTRACT or DIVIDE', () => {
-    expect(() => calculateNumber('MULTIPLY', 1, 3)).to.throw(TypeError, 'Invalid type. Use SUM, SUBTRACT or DIVIDE');
+
+  it('should return "Error" when attempting to divide by 0', () => {
+    const result = calculateNumber('DIVIDE', 1.4, 0);
+    expect(result).to.equal('Error');
   });
-  it('should throw an error if the second number is 0', () => {
-    expect(calculateNumber('DIVIDE', 1, 0)).to.equal('Error');
+
+  it('should throw an error for an invalid type', () => {
+    expect(() => {
+      calculateNumber('INVALID', 1.4, 4.5);
+    }).to.throw('Invalid type. Use SUM, SUBTRACT or DIVIDE');
   });
 });
