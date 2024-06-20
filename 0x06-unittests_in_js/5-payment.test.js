214f8eaedc2fe8d195fbaf48635a1a05f09ccaf1
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-06-21 01:10:03 +0300

    update 5

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -4,16 +4,21 @@ const sendPaymentRequestToApi = require('./5-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
-  it('should call Utils.calculateNumber', () => {
-    const spy = sinon.spy(Utils, 'calculateNumber');
-    sendPaymentRequestToApi(100, 20);
-    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-    spy.restore();
+  let logSpy;
+  beforeEach(() => {
+    logSpy = sinon.spy(console, 'log');
   });
-  it('should call Utils.calculateNumber', () => {
-    const spy = sinon.spy(Utils, 'calculateNumber');
+  afterEach(() => {
+    logSpy.restore();
+  });
+  it('should log the correct output when called with 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(logSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+    expect(logSpy.calledOnce).to.be.true;
+  });
+  it('should log the correct output when called with 10 and 10', () => {
     sendPaymentRequestToApi(10, 10);
-    expect(spy.calledOnceWithExactly('SUM', 10, 10)).to.be.true;
-    spy.restore();
+    expect(logSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+    expect(logSpy.calledOnce).to.be.true;
   });
 });
