fa4f9b3d5be0fe0bda42e152367104135cceb0a8
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-06-21 01:06:46 +0300

    update 4

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -5,17 +5,12 @@ const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
   it('should call Utils.calculateNumber', () => {
-    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
-    calculateNumberStub.returns(10);
-
-    const logSpy = sinon.spy(console, 'log');
-    const res = sendPaymentRequestToApi(100, 20);
-
-    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-    expect(logSpy.calledWithExactly('The total is: 10')).to.be.true;
-    expect(res).to.equal(10);
-
-    calculateNumberStub.restore();
-    logSpy.restore();
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(console.log.calledOnceWithExactly('The total is: 10')).to.be.true;
+    spy.restore();
+    stub.restore();
   });
 });
