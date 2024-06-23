45139d17e7e291e481ed0bc3b5e557d76c862af0
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-06-23 19:33:31 +0300

    add test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,6 +5,7 @@ describe('calculateNumber', function () {
   it('should round two numbers and return their sum', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.7, 3), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
