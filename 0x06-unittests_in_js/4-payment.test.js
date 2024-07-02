5bad09cb6d153e39c26504fc1400b855e34e9144
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-07-03 00:52:51 +0300

    update test 4

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,16 +1,20 @@
-const { expect } = require('chai');
 const sinon = require('sinon');
-const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('should call Utils.calculateNumber', () => {
-    const spy = sinon.spy(Utils, 'calculateNumber');
-    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+  it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
+    const bigBrother = sinon.spy(console);
+    const dummy = sinon.stub(Utils, 'calculateNumber');
+
+    dummy.returns(10);
     sendPaymentRequestToApi(100, 20);
-    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-    expect(console.log.calledOnceWithExactly('The total is: 10')).to.be.true;
-    spy.restore();
-    stub.restore();
+    expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(dummy.callCount).to.be.equal(1);
+    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
+    expect(bigBrother.log.callCount).to.be.equal(1);
+    dummy.restore();
+    bigBrother.log.restore();
   });
 });
