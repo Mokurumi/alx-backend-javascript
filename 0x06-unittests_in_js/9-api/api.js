57c2449325c97a0d9292914b26b4d3be7decee9c
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-07-05 01:15:48 +0300

    update 9

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,23 +1,20 @@
 const express = require('express');
 
 const app = express();
+const PORT = 7865;
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id', (req, res) => {
-  const { id } = req.params;
-  if (isNaN(id)) {
-    res.status(400).send('ID must be a number');
-  } else {
-    res.send(`Payment methods for cart ${id}`);
-  }
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
 });
 
-app.listen(7865, () => {
-  console.log('API available on localhost port 7865');
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
 
 module.exports = app;
-
